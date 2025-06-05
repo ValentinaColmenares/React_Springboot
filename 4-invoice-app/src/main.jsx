470e13306464f9e805c35ffcb5a266fde86c46d9
juanesrp <juanesrp@gmail.com>
2025-06-05 17:11:47 -0500

    app 3 y 4 incompleta

---
+++ 4-invoice-app/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { InvoiceApp } from './components/InvoiceApp';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <InvoiceApp />
+  </StrictMode>,
+)
