9c0816f43f591a1ed4135021f74027211ec6d0ee
Valentina <vale_colmenares@hotmail.com>
2025-06-15 21:14:21 -0500

    Hasta clase 82

---
+++ 5-cart-app/src/data/products.js
@@ -0,0 +1,38 @@
+export const products = [
+	{
+		id: 1,
+		name: 'Teclado Mecánico RGB',
+		description: 'Teclado mecánico con luces RGB switches cherry red',
+		price: 1000 
+	},
+	{
+		id: 2,
+		name: 'Samsung Smart TV 55',
+		description: 'Excelente tv LCD...',
+		price: 3000 
+	},
+	{
+		id: 3,
+		name: 'Audifono Bluetooth Sony',
+		description: 'Audifono Bluetooth para smarthphone',
+		price: 770 
+	},
+	{
+		id: 4,
+		name: 'Memoria Corsair 8GB DDR5',
+		description: 'Memoria RAM optimizada para Juegos',
+		price: 3700 
+	},
+	{
+		id: 5,
+		name: 'Asus Nvidia RTX',
+		description: 'Tarjeta Gráfica para juegos en 4k',
+		price: 5000 
+	},
+	{
+		id: 6,
+		name: 'CPU Intel Core i5',
+		description: 'Optimizada para tareas multicore de 12va generación',
+		price: 4000 
+	},
+]
