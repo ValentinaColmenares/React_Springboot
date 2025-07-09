9057894e7119bfcdfdfe124f68b3e76e9c6f236d
ValentinaColmenares <vale_colmenares@hotmail.com>
2025-07-09 17:10:20 -0500

    Hasta 195

---
+++ Spring/backend-usersapp/src/main/java/com/valentina/backend/usersapp/backend_usersapp/auth/SpringSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.valentina.backend.usersapp.backend_usersapp.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+        .authorizeHttpRequests(authRules -> authRules
+            .requestMatchers(HttpMethod.GET, "/users").permitAll()
+            .anyRequest().authenticated()
+        )
+        .csrf(config -> config.disable()) // En APIREST se tiene que desabilita
+        .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .build();
+    }
+}
---
+++ Spring/backend-usersapp/src/main/java/com/valentina/backend/usersapp/backend_usersapp/auth/filters/JwtAuthenticationFilter.java
@@ -0,0 +1,61 @@
+package com.valentina.backend.usersapp.backend_usersapp.auth.filters;
+
+import java.io.IOException;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.valentina.backend.usersapp.backend_usersapp.models.entities.User;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter{
+
+    private AuthenticationManager authenticationManager;
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+        throws AuthenticationException {
+            
+            User user = null;
+            String username = null;    
+            String password = null;
+
+            try {
+                user = new ObjectMapper().readValue(request.getInputStream(), User.class);
+                username = user.getUsername();
+                password = user.getPassword();
+                
+            } catch (StreamReadException e) {
+                e.printStackTrace();
+            } catch (DatabindException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            
+            return authenticationManager.authenticate(null);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+            Authentication authResult) throws IOException, ServletException {
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+            AuthenticationException failed) throws IOException, ServletException {
+    }
+    
+}
